bbdc394775b0cb9bae71e5ca7f2e946c395cec11
agungfp <agungjunior541@gmail.com>
2020-08-23 00:13:12 +0700

    convert HTML to svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,14 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            pledged: 100000,
+            target: 100000,
+            date_end: +new Date('30 August 2020'),
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+        }
+    ]
+}
