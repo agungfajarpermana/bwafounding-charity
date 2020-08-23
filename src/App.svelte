340b908457300837ce9efbc9c7dc33b28a507fb1
agungfp <agungjunior541@gmail.com>
2020-08-23 18:06:48 +0700

    Added template not found svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
 	import About from './pages/About.svelte';
 	import Contact from './pages/Contact.svelte';
 	import Donation from './pages/Donation.svelte';
+	import NotFound from './pages/NotFound.svelte';
 
 	let page, params;
 
@@ -14,6 +15,7 @@
 		params = ctx.params;
 		next();
 	}, () => (page = Donation));
+	router('/*', () => (page = NotFound));
 
 	router.start();
 </script>
