61508a7c959f1ce4267cf1e6febf59ebd8ab8fa0
agungfp <agungjunior541@gmail.com>
2020-08-30 17:52:26 +0700

    modifiy paymetn in production

---
+++ src/components/Map.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {onMount} from 'svelte';
+    import {mapOptions} from '../config';
+
+    let container, map;
+
+    onMount(() => {
+        map = new google.maps.Map(container, mapOptions);
+        let marker = new google.maps.Marker({
+            position: new google.maps.LatLng(40.6700,-73.9400),
+            map,
+            title: 'Snazzy!'
+        })
+    })
+</script>
+
+<div class="xs-maps-wraper map-wraper-v2">
+    <div id="xs-map" class="xs-box-shadow-2" bind:this={container}></div>
+</div>
+
+<style>
+
+</style>
